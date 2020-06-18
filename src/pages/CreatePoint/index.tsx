bac3db5fff6e177598b0b9602feeea4a23618de6
Jonatas Rafael Carneiro Ribeiro <jonatasrafael@jonatasrafael.com>
2020-06-18 00:34:43 -0300

    Added images upload

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -5,6 +5,7 @@ import { Map, TileLayer, Marker } from 'react-leaflet';
 import axios from 'axios';
 import { LeafletMouseEvent } from 'leaflet';
 import api from '../../services/api';
+import Dropzone from '../../components/Dropzone';
 
 import './styles.css';
 
@@ -35,6 +36,7 @@ const CreatePoint = () => {
   const [selectedItems, setSelectedItems] = useState<number[]>([]);
   const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
   const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
+  const [selectedFile, setSelectedFile] = useState<File>();
 
   const [formData, setFormData] = useState({
     name: '',
@@ -127,16 +129,20 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items
-    };
+    const data = new FormData();
+
+    data.append('name',name);
+    data.append('email',email);
+    data.append('whatsapp',whatsapp);
+    data.append('uf',uf);
+    data.append('city',city);
+    data.append('latitude',String(latitude));
+    data.append('longitude',String(longitude));
+    data.append('items',items.join(','));
+
+    if (selectedFile) {
+      data.append('image', selectedFile);
+    }
 
     await api.post('points', data);
 
@@ -159,6 +165,8 @@ const CreatePoint = () => {
       <form onSubmit={handleSubmit}>
         <h1>Cadastro do<br /> ponto de coleta</h1>
 
+        <Dropzone onFileUploaded={setSelectedFile} />
+
         <fieldset>
           <legend>
             <h2>Dados</h2>
@@ -171,7 +179,7 @@ const CreatePoint = () => {
           </div>
           <div className="field-group">
             <div className="field">
-              <label htmlFor="email">E-maiol</label>
+              <label htmlFor="email">E-mail</label>
               <input onChange={handleInputChange} type="email" name="email" id="email" />
 
             </div>
